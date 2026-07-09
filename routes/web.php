af3df376f154ab9a51e3bdde1a5d9cf7728583fe
JefreeMauricio <jefreegoficial+1@gmail.com>
2026-07-09 13:21:36 -0500

    refactor: Ejecutar nueva base de datos  con SQLLITE

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
 
 Route::get('/', [ChirpController::class, 'index'])->name('chirps.index');
 Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit'])->name('chirps.edit');
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update'])->name('chirps.update');
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy'])->name('chirps.destroy');
+
